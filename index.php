132a83c6c47bdf7ae19acab90b1feff042756793
JustineLcode <leulierjustine@hotmail.com>
2025-02-19 16:18:49 +0100

    Question sur la logique

--- index.php
+++ index.php
@@ -5,12 +5,29 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$dzd = $_GET["dzd"]; // Conversion dinar Algérien en euro
-$dzd_to_euro = floatval($dzd) / 140.82;
-$dzd_to_euro_round = round($dzd_to_euro, 2);
 
-$euro= $_GET["euro"]; // Conversion euro en dinar Algérien
-$euro_to_dzd = floatval($euro) * 140.82;
-$euro_to_dzd_round = round($euro_to_dzd, 2);
+if (isset($_GET["option-from"]) && isset($_GET["option-to"]) && isset($_GET["amount"])) {
+    $option_from = $_GET["option-from"];
+    $option_to = $_GET["option-to"];
+    $amount = $_GET["amount"];
+    
+    if ($option_from === "EUR" && $option_to === "DZD") {
+        $euro_to_dzd = floatval($amount) * 140.82;
+        $euro_to_dzd_round = round($euro_to_dzd, 2);
+        } else if ($option_from === "DZD" && $option_to === "EUR") {
+            $dzd_to_euro = floatval($amount) / 140.82;
+            $dzd_to_euro_round = round($dzd_to_euro, 2);
+            } else if ($option_from === "EUR" && $option_to === "USD"){
+                $euro_to_usd = floatval($amount) * 1.04;
+                $euro_to_usd_round = round($euro_to_usd, 2);
+            } else if ($option_from === "USD" && $option_to === "EUR"){
+                    $usd_to_eur = floatval($amount) / 1.04;
+                    $usd_to_eur_round = round($usd_to_eur, 2);
+            }
+}
+
+
+
 require './resources/views/home.php';
+
 ?>
--- resources/views/home.php
+++ resources/views/home.php
@@ -1,23 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Dream Project</title>
     <link rel="stylesheet" href="../The-Dream-project/resources/css/home.css">
 </head>
+
 <body>
-<div class="login-container">
-        <h2>Currency converter</h2>
-        <form method="GET" action="index.php">
-            <input type="number" placeholder="€" name="euro">
-            <button type="submit">&rarr;</button>
-            <p><?php echo $euro_to_dzd_round ?> DZD</p>
-            <br>
-            <input type="number" placeholder="DZD" name="dzd">
-            <button type="submit">&rarr;</button>
-            <p><?php echo $dzd_to_euro_round ?> €</p>
-        </form>
-    </div>
+    <h2>Currency converter</h2>
+    <form class="form-container" method="GET" action="index.php">
+        <div class="form-group">
+            <select class="dropdown" name="option-from">
+                <option name="EUR">EUR</option>
+                <option name ="DZD">DZD</option>
+                <option>USD</option>
+                <option>YEN</option>
+                <option>SKR</option>
+            </select>
+
+            <button class="switch-button">SWITCH</button>
+
+            <select class="dropdown" name="option-to">
+                <option name="EUR">EUR</option>
+                <option name="DZD">DZD</option>
+                <option>USD</option>
+                <option>YEN</option>
+                <option>SKR</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label class="label" for="amount" >Amount</label>
+            <input type="number" id="amount" placeholder="Enter amount" class="input-field" name="amount"/>
+
+        </div>
+
+        <button class="convert-button">Convert</button>
+        <p><?php echo $euro_to_dzd_round ?> DZD</p>
+        <p><?php echo $dzd_to_euro_round ?> €</p>
+        <p><?php echo $euro_to_usd_round ?> USD</p>
+        <p><?php echo $usd_to_eur_round ?> €</p>
+    </form>
 </body>
 </html>
